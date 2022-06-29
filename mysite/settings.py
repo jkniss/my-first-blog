4ff48ac14008611b117521617a420ed0e2d82c7c
jkniss <jessekniss05@gmail.com>
2022-06-29 15:59:22 -0600

    adding key variables

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i@4swgix9nhjil=16ta!s@wt@)c7%z=1belerl5es74la!g0*k'
+with open('keys.txt') as f:
+    SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
