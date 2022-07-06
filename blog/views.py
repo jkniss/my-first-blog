739d92631f784d5931a9e138bc1d9077a1dcbfb8
jkniss <jessekniss05@gmail.com>
2022-07-05 18:00:30 -0600

    Added templates for post list and post detail

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -10,4 +11,10 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     # this returns a template file with the information recieved from the user via internet.
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+
+    # variable to hold the queryset
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
